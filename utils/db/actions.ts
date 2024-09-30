0ce624c9f0bd723533b1ad98be175f9d09070b16
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-30 23:50:28 +0530

    Refactor database actions and add user balance calculation

---
+++ utils/db/actions.ts
@@ -0,0 +1,81 @@
+import { db } from "./dbConfig";
+import { Notifications, Transactions, Users } from "./schema";
+import { eq, sql, and, desc } from "drizzle-orm";
+
+export async function createUser(email: string, name: string) {
+  try {
+    const [user] = await db
+      .insert(Users)
+      .values({ email, name })
+      .returning()
+      .execute();
+    return user;
+  } catch (error) {
+    console.error("Error creating user: ", error);
+    return null;
+  }
+}
+
+export async function getUserByEmail(email: string) {
+  try {
+    const [user] = await db
+      .select()
+      .from(Users)
+      .where(eq(Users.email, email))
+      .execute();
+    return user;
+  } catch (error) {
+    console.error("Error fetching user by email: ", error);
+    return null;
+  }
+}
+
+export async function getUnreadNotifications(userId: number) {
+  try {
+    return await db
+      .select()
+      .from(Notifications)
+      .where(
+        and(eq(Notifications.userId, userId), eq(Notifications.isRead, false))
+      )
+      .execute();
+  } catch (error) {
+    console.error("Error fetching unread notifications: ", error);
+    return null;
+  }
+}
+
+export async function getRewardTransactions(userId: number) {
+  try {
+    const transactions = await db
+      .select({
+        id: Transactions.id,
+        type: Transactions.type,
+        amount: Transactions.amount,
+        description: Transactions.description,
+        date: Transactions.date,
+      })
+      .from(Transactions)
+      .where(eq(Transactions.userId, userId))
+      .orderBy(desc(Transactions.date))
+      .limit(10)
+      .execute();
+  } catch (error) {
+    console.error("Error fetching reward transactions: ", error);
+    return null;
+  }
+}
+
+export async function getUserBalance(userId: number): Promise<number> {
+  const transactions = (await getRewardTransactions(userId)) || [];
+
+  if (!transactions) return 0;
+
+  const balance = transactions.reduce((acc: number, transaction: any) => {
+    return transaction.type.startsWith("earned")
+      ? acc + transaction.amount
+      : acc - transaction.amount;
+  }, 0);
+
+  return Math.max(balance, 0);
+}
