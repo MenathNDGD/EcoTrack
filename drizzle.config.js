353b31f9c83786f88da391512ce68e55762f82f7
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-30 15:41:57 +0530

    Add drizzle configuration file

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/db/schema.ts",
+  out: "./drizzle",
+
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
+  },
+};
