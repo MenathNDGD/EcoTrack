3265659efedbb3ddc85a21e73be9bf06fc8bd81f
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-01 21:27:56 +0530

    Update database environment variables

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,7 +4,7 @@ export default {
   out: "./drizzle",
 
   dbCredentials: {
-    url: process.env.DATABASE_URL,
-    connectionString: process.env.DATABASE_URL,
+    url: process.env.NEXT_PUBLIC_DATABASE_URL,
+    connectionString: process.env.NEXT_PUBLIC_DATABASE_URL,
   },
 };
