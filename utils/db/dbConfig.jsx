37db7cc335b1fe3907734489e3f8a54cf7b9a56b
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-01 21:31:14 +0530

    Refactor database configuration to use the correct environment variable

--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -3,6 +3,6 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
 
 export const db = drizzle(sql, { schema });
